03071b6ff6836d48cb6a77c6c65a8e819e6cfc92
Dessert99 <lsjh1234@naver.com>
2025-10-25 21:11:49 +0900

    feat(nav): 홈, 오늘, 마이페이지 nav 초기 구조
    - 구현 탭은 홈, 오늘, 마이페이지이다.
    - 오늘 탭에서는 검색과 알림 페이지로 이동할 수 있다. (스택)

---
+++ app/(tabs)/today/notification.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function NotificationScreen() {
+  return (
+    <View>
+      <Text>알림</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default NotificationScreen;
