652eb557ae2506f13c54b5f359ab8e39f2ac1ee9
Dessert99 <lsjh1234@naver.com>
2025-10-27 12:08:36 +0900

    feat: 임시로 마이페이지에 로그아웃 버튼을 구현

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { useLogout } from "@/hooks/queries/useAuth";
+import { Pressable, Text, View } from "react-native";
 
 export default function MyScreen() {
+  const logoutMutation = useLogout();
+
   return (
     <View>
       <Text>마이페이지</Text>
+      <Pressable
+        onPress={() => {
+          logoutMutation.mutate();
+        }}
+      >
+        <Text>로그아웃</Text>
+      </Pressable>
     </View>
   );
 }
