7cbe35b392c8f22f3446682d7f14c8ce443a86b2
Dessert99 <lsjh1234@naver.com>
2025-10-27 10:12:50 +0900

    chore: console.log지우고 로깅 유틸 함수로 변경

--- hooks/queries/useAuth.ts
+++ hooks/queries/useAuth.ts
@@ -1,5 +1,4 @@
-import { postLogin } from "@/api/auth";
-import { saveSecureStore } from "@/utils/secureStore";
+import { logOnDev } from "@/utils/logOnDev";
 import { useMutation } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { Alert } from "react-native";
@@ -10,11 +9,12 @@ export function useLogin() {
     onSuccess: async ({ accessToken, refreshToken }) => {
       await saveSecureStore("accessToken", accessToken);
       await saveSecureStore("refreshToken", refreshToken);
-      console.log("로그인 성공");
+
+      logOnDev("로그인 성공!");
       router.replace("/");
     },
     onError: (error) => {
-      console.error(error);
+      logOnDev(`로그인 에러: ${error.message}`);
       Alert.alert("이메일과 비밀번호를 확인해주세요.");
     },
   });
