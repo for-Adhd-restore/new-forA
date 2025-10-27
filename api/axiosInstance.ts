fe9d8f554de4dc8ab490a3753a0bbc8007de92d2
Dessert99 <lsjh1234@naver.com>
2025-10-28 00:32:29 +0900

    feat(auth): axios response interceptors 추가
    - 에러를 가로채 status case에 따라 로직을 실행한다.
    - 현재 401에러만 처리한다.
    - 토큰 재발급 후 재요청 보낸다.

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,6 +1,12 @@
 import { logOnDev } from "@/utils/logOnDev";
 import { getSecureStore } from "@/utils/secureStore";
-import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { Alert } from "react-native";
+import { tokenRefresh } from "./tokenRefresh";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
@@ -29,6 +35,48 @@ axiosInstance.interceptors.request.use(
   },
   function (error: AxiosError) {
     logOnDev(`[API request error]: ${error.config}`);
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  function (response: AxiosResponse) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  async function (error: AxiosError) {
+    const { response } = error;
+    const originalRequest = response?.config;
+    switch (response?.status) {
+      case 401:
+        logOnDev("401인증 오류");
+        // 감싸줘야 타입 오류 안 생김
+        if (originalRequest) {
+          try {
+            await tokenRefresh();
+            const accessToken = await getSecureStore("accessToken");
+            originalRequest?.headers.set(
+              "Authorization",
+              `Bearer ${accessToken}`,
+            );
+            return axiosInstance(originalRequest);
+          } catch (e) {
+            return Promise.reject(e);
+          }
+        }
+        break;
+
+      case 403:
+        Alert.alert("접근 권한이 없습니다.");
+        break;
+      case 500:
+        Alert.alert("서버 오류가 발생했습니다.");
+        break;
+      default:
+        logOnDev(`Unhandled error:  ${error.response?.data}`);
+    }
+
     return Promise.reject(error);
   },
 );
