c554b2c5ed01d14e7bad47d88e34826ce66f5b8d
Dessert99 <lsjh1234@naver.com>
2025-10-27 12:07:26 +0900

    feat(auth): 로그아웃 쿼리훅 추가
    - SecureStore에서 토큰을 지워준다.
    - 사용자 경험 개선을 위해 onSettled로 설정해 서버 상황과 관계없이 클라이언트는 로그인해준다.

--- hooks/queries/useAuth.ts
+++ hooks/queries/useAuth.ts
@@ -1,6 +1,7 @@
 import { logout, postLogin } from "@/api/auth";
 import { queryClient } from "@/api/queryClient";
 import { logOnDev } from "@/utils/logOnDev";
+import { deleteSecureStore, saveSecureStore } from "@/utils/secureStore";
 import { useMutation } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { Alert } from "react-native";
@@ -18,6 +19,19 @@ export function useLogin() {
     onError: (error) => {
       logOnDev(`로그인 에러: ${error.message}`);
       Alert.alert("이메일과 비밀번호를 확인해주세요.");
+    },
+  });
+}
+
+export function useLogout() {
+  return useMutation({
+    mutationFn: logout,
+    onSettled: async () => {
+      await deleteSecureStore("accessToken");
+      await deleteSecureStore("refreshToken");
+      queryClient.resetQueries({ queryKey: ["auth"] });
+      logOnDev("로그아웃");
+      router.replace("/auth/login");
     },
   });
 }
