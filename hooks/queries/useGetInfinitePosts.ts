6ba7c63f6fcc767b60b2e4aedb95de61c81cfbac
Dessert99 <lsjh1234@naver.com>
2025-10-29 21:33:41 +0900

    feat(post): 오늘탭 게시글 무한스크롤 로직 useGetInfinitePosts 구현
    - category별로 queryKey키를 다르게 설정

---
+++ hooks/queries/useGetInfinitePosts.ts
@@ -0,0 +1,15 @@
+import { getPosts } from "@/api/post";
+import { PostCategory } from "@/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+export function useGetInfinitePosts(category: PostCategory) {
+  return useInfiniteQuery({
+    queryFn: ({ pageParam }) => getPosts({ pageParam, category }),
+    queryKey: ["posts", category], // category 값이 변경될 때마다 React Query가 이를 "새로운 쿼리"로 인식하고 해당 카테고리의 데이터를 자동으로 다시 가져온다.
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.paging.isLast) return undefined;
+      return allPages.length;
+    },
+  });
+}
