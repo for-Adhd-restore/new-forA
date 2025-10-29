994a90c1b61ff0301c1836ed7f25fd6f26b86194
Dessert99 <lsjh1234@naver.com>
2025-10-29 21:37:08 +0900

    feat(post): 오늘탭 index페이지에 FeedList연결

--- app/(tabs)/today/index.tsx
+++ app/(tabs)/today/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import FeedList from "@/components/FeedList";
+import { PostCategory } from "@/types";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function TodayScreen() {
+  const [selectedCategory, setSelectedCategory] =
+    useState<PostCategory>("TWENTIES");
   return (
-    <View>
-      <Text>오늘탭</Text>
+    <View style={styles.container}>
+      <FeedList selectedCategory={selectedCategory} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+  },
+});
