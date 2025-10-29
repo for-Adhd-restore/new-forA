c7d55e593ec86259e99defcd2b34d38ec9432c29
Dessert99 <lsjh1234@naver.com>
2025-10-29 21:30:25 +0900

    feat(post): 오늘탭 무한 스크롤에 사용될 타입 추가
    - 카테고리 타입
    - 게시글, 게시글 상세 등

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,90 @@ export interface AuthTokens {
   accessToken: string;
   refreshToken: string;
 }
+
+//오늘
+
+export type PostCategory =
+  | "TEENS"
+  | "TWENTIES"
+  | "THIRTIES_AND_ABOVE"
+  | "PARENTS"
+  | "NOTICE";
+
+export interface Paging {
+  page: number;
+  size: number;
+  totalPages: number;
+  numberOfElements: number;
+  totalElements: number;
+  isFirst: boolean;
+  isLast: boolean;
+  isEmpty: boolean;
+}
+
+export interface Post {
+  id: number;
+  userId: string;
+  title: string;
+  content: string;
+  anonymous: boolean;
+  images: string[] | null;
+  isLiked?: boolean;
+  isScrapped?: boolean;
+  likeCount: number;
+  commentCount: number;
+  scrapCount: number;
+  viewCount: number;
+  category: PostCategory;
+  nickname: string | null;
+  profileImage: string | null;
+  createdAt: number;
+  lastModifiedAt: number;
+  isAuthor?: boolean;
+}
+
+export type Comment = {
+  id: number;
+  content: string;
+  userId: string;
+  postId: number;
+  anonymous: boolean;
+  likeCount: number;
+  createdAt: number;
+  lastModifiedAt: number;
+  parentCommentId: number | null;
+  children: Comment[];
+  isLiked: boolean;
+  nickname: string;
+  isCommentAuthor: boolean;
+  isBlocked: boolean;
+  profileImage: string;
+};
+
+export type PostDetail = {
+  id: number;
+  userId: string;
+  title: string;
+  content: string;
+  anonymous: boolean;
+  images: string[] | null;
+  isLiked?: boolean;
+  isScrapped?: boolean;
+  likeCount: number;
+  commentCount: number;
+  scrapCount: number;
+  viewCount: number;
+  category: string;
+  comments: Comment[];
+  nickname: string | null;
+  profileImage: string | null;
+  createdAt: number;
+  lastModifiedAt: number;
+  isAuthor?: boolean;
+  isBlocked?: boolean;
+};
+
+export interface PostList {
+  postList: Post[];
+  paging: Paging;
+}
