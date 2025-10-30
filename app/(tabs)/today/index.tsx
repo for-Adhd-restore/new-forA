4a9669af04971501bc23857beb31987bef975a6e
Dessert99 <lsjh1234@naver.com>
2025-10-30 18:29:33 +0900

    feat(post): 오늘탭 메인에 카테고리 버튼 연결

--- app/(tabs)/today/index.tsx
+++ app/(tabs)/today/index.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButton";
 import FeedList from "@/components/FeedList";
 import { PostCategory } from "@/types";
 import { useState } from "react";
@@ -8,6 +9,24 @@ export default function TodayScreen() {
     useState<PostCategory>("TWENTIES");
   return (
     <View style={styles.container}>
+      <View style={styles.categoryContainer}>
+        <CustomButton
+          label="10대"
+          onPress={() => setSelectedCategory("TEENS")}
+        />
+        <CustomButton
+          label="20대"
+          onPress={() => setSelectedCategory("TWENTIES")}
+        />
+        <CustomButton
+          label="30대"
+          onPress={() => setSelectedCategory("THIRTIES_AND_ABOVE")}
+        />
+        <CustomButton
+          label="학부모"
+          onPress={() => setSelectedCategory("PARENTS")}
+        />
+      </View>
       <FeedList selectedCategory={selectedCategory} />
     </View>
   );
@@ -17,5 +36,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderWidth: 1,
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    gap: 10,
   },
 });
