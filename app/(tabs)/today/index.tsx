be40bfda9a867c07e7c9e5298c84294a952c102d
Dessert99 <lsjh1234@naver.com>
2025-11-01 14:42:25 +0900

    feat(post): CategoryButtonList컴포넌트 구현
    - TodayScreen에서 카테고리 상태를 바꾸는 버튼 리스트를 컴포넌트로 분리했다.
    - setSelectedCategory 세터 함수를 props로 내려주며 이벤트 발생 시 부모로 올려준다.

--- app/(tabs)/today/index.tsx
+++ app/(tabs)/today/index.tsx
@@ -1,4 +1,4 @@
-import CustomButton from "@/components/CustomButton";
+import CategoryButtonList from "@/components/CategoryButtonList";
 import FeedList from "@/components/FeedList";
 import { PostCategory } from "@/types";
 import { Link } from "expo-router";
@@ -8,26 +8,10 @@ import { Image, Pressable, StyleSheet, View } from "react-native";
 export default function TodayScreen() {
   const [selectedCategory, setSelectedCategory] =
     useState<PostCategory>("TWENTIES");
+
   return (
     <View style={styles.container}>
-      <View style={styles.categoryContainer}>
-        <CustomButton
-          label="10대"
-          onPress={() => setSelectedCategory("TEENS")}
-        />
-        <CustomButton
-          label="20대"
-          onPress={() => setSelectedCategory("TWENTIES")}
-        />
-        <CustomButton
-          label="30대"
-          onPress={() => setSelectedCategory("THIRTIES_AND_ABOVE")}
-        />
-        <CustomButton
-          label="학부모"
-          onPress={() => setSelectedCategory("PARENTS")}
-        />
-      </View>
+      <CategoryButtonList setSelectedCategory={setSelectedCategory} />
       <FeedList selectedCategory={selectedCategory} />
       <Link href={"/post/write"} asChild>
         <Pressable style={styles.writeButton}>
@@ -45,10 +29,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderWidth: 1,
-  },
-  categoryContainer: {
-    flexDirection: "row",
-    gap: 10,
   },
   writeButton: {
     position: "absolute",
