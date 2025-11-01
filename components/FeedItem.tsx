2a1536c756987591740bcd220a4a6fd5e2088b80
Dessert99 <lsjh1234@naver.com>
2025-11-01 15:24:53 +0900

    chore: FeedItem 컨테이너를 View에서 Pressable로 변경
    - 클릭하면, 게시글 id로 동적 라우팅한다.

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -1,8 +1,9 @@
 import { colors } from "@/constants/colors";
 import { Post } from "@/types";
 import { unixToUTCString } from "@/utils/date";
+import { router } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 interface FeedItemProps {
   post: Post;
@@ -10,14 +11,17 @@ interface FeedItemProps {
 
 function FeedItem({ post }: FeedItemProps) {
   return (
-    <View style={styles.container}>
+    <Pressable
+      style={styles.container}
+      onPress={() => router.push(`/post/${post.id}`)}
+    >
       <Text>닉네임 : {post.nickname}</Text>
       <Text>제목 : {post.title}</Text>
       <Text>프로필 이미지 : {post.profileImage}</Text>
       <Text>카테고리 : {post.category}</Text>
       <Text>첨부 이미지 : {post.images}</Text>
       <Text>생성 일자 : {unixToUTCString(post.createdAt)}</Text>
-    </View>
+    </Pressable>
   );
 }
 
