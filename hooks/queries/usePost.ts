a465287888bf87810e4c59495ef3d092f0e983f7
Dessert99 <lsjh1234@naver.com>
2025-11-01 15:23:56 +0900

    feat(post): useGetPost훅 추가
    - 개별 게시물 조회하는 쿼리 훅이다.
    - enabled: !isNaN(id) 옵션을 추가하여 id값이 유효할 때만 실행한다.

--- hooks/queries/usePost.ts
+++ hooks/queries/usePost.ts
@@ -1,6 +1,7 @@
-import { createPost } from "@/api/post";
+import { createPost, getPost } from "@/api/post";
 import { queryClient } from "@/api/queryClient";
 import { queryKeys } from "@/constants/queryKeys";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { router } from "expo-router";
 
 export function useCreatePost() {
@@ -12,5 +13,13 @@ export function useCreatePost() {
         queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
       });
     },
+  });
+}
+
+export function useGetPost(id: number) {
+  return useQuery({
+    queryFn: () => getPost(id),
+    queryKey: [queryKeys.POST, queryKeys.GET_POST, id],
+    enabled: !isNaN(id), // id가 NaN이 아닐 때만 실행. id 값이 유효해질 때, 즉, URL 파라미터가 제대로 들어올 때 API 요청
   });
 }
