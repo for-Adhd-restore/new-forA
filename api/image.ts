ffaf15aedfea90d1bb6e44ba5fd1bd9c3cf98424
Dessert99 <lsjh1234@naver.com>
2025-11-01 14:00:35 +0900

    feat(image): s3 이미지 업로드 api 구현
    - uir, filename, mimeType 정보가 담긴 이미지 리스트를 매개변수로 받는다.
    - 폼 데이터를 명세서에 맞게 구성한다.
    - 반환값인 이미지 경로 접두어로 CLOUDFRONT_URL를 더해 완전한 uri 경로를 배열로 반환한다.

---
+++ api/image.ts
@@ -0,0 +1,29 @@
+import { ImageData } from "@/types";
+import { axiosInstance } from "./axiosInstance";
+
+export const uploadImages = async (
+  imageUris: ImageData[],
+): Promise<string[]> => {
+  const formData = new FormData();
+
+  imageUris.forEach((image) => {
+    formData.append("imageFileList", {
+      uri: image.uri,
+      name: image.fileName,
+      type: image.mimeType,
+    } as any); // RN FormData 타입 이슈로 as any 필요
+  });
+
+  formData.append(
+    "request",
+    JSON.stringify({ imagePathPrefix: "DEFAULT_IMAGE" }),
+  );
+  const { data } = await axiosInstance.post("/files/images", formData);
+  // 반환 예시 data -> {"imagePathList": ["image/6bf768e5c7c844d28202d22214f4a87c_3000x2002.jpg"]}
+
+  const newImagePathList = data.imagePathList.map(
+    (imagePath: string) =>
+      process.env.EXPO_PUBLIC_CDN_CLOUDFRONT_URL + "/" + imagePath,
+  );
+  return newImagePathList; // uri배열
+};
