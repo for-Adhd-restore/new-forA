f455784185803b4e23d79a66278ead147fe38463
Dessert99 <lsjh1234@naver.com>
2025-11-09 14:19:29 +0900

    feat(my): 마이페이지-나의 글 페이지 구현
    - 카테고리별로 무한 스크롤
    - InfiniteList에 useGetInfiniteMyPosts값을 주입한다.
    
    #7

---
+++ app/(tabs)/my/myPost.tsx
@@ -0,0 +1,51 @@
+import CategoryButtonList from "@/components/CategoryButtonList";
+import InfiniteList from "@/components/common/InfiniteList";
+import FeedItem from "@/components/FeedItem";
+import MyPostEmpty from "@/components/my/MyPostEmpty";
+import { useGetInfiniteMyPosts } from "@/hooks/queries/useGetInfinitePosts";
+import { Post, PostCategory } from "@/types";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function MyPostScreen() {
+  const [selectedCategory, setSelectedCategory] =
+    useState<PostCategory>("TWENTIES");
+
+  const {
+    data,
+    error,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetInfiniteMyPosts(selectedCategory);
+
+  // 페이지 풀기
+  //TODO: 나중에 util로 빼야 하나?
+  const flatPosts: Post[] =
+    data?.pages.map((item) => item.postList).flat() ?? [];
+
+  return (
+    <View style={styles.container}>
+      <CategoryButtonList setSelectedCategory={setSelectedCategory} />
+
+      <InfiniteList
+        data={flatPosts}
+        error={error}
+        isLoading={isLoading}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isFetchingNextPage={isFetchingNextPage}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <FeedItem post={item} />}
+        ListEmptyComponent={MyPostEmpty}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // 화면을 꽉 채우기 위해서
+  },
+});
