40432f778f969717aeb464592cdfc8d2f5ff53d8
Dessert99 <lsjh1234@naver.com>
2025-11-09 14:14:16 +0900

    fix(query): staleTime을 1분에서 5분으로 변경
    - 무한 스크롤 경고 제거를 위함

--- api/queryClient.ts
+++ api/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1 * 60 * 1000,
+      staleTime: 5 * 60 * 1000,
       gcTime: 5 * 60 * 1000,
       retry: 1,
       refetchOnReconnect: true, // 앱이 다시 온라인이면 재요청 허용
