9551a030f6e7fd412fc4d737c3b6d27f647327bb
Dessert99 <lsjh1234@naver.com>
2025-11-10 13:29:53 +0900

    feat(my): 마이페이지-나의 댓글 페이지 구현
    - useGetInfiniteMyComments훅에서 꺼낸 옵션들을 InfiniteList에 주입시킨다.
    - 조회된 댓글이 없는 경우 MyCommentEmpty를 렌더링 시킨다.
    
    #7

---
+++ app/(tabs)/my/myComment.tsx
@@ -0,0 +1,43 @@
+import InfiniteList from "@/components/common/InfiniteList";
+import FeedItem from "@/components/FeedItem";
+import MyCommentEmpty from "@/components/my/MyCommentEmpty";
+import { useGetInfiniteMyComments } from "@/hooks/queries/useGetInfiniteComments";
+import { Post } from "@/types";
+import { StyleSheet, View } from "react-native";
+
+export default function MyCommentScreen() {
+  const {
+    data,
+    error,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetInfiniteMyComments();
+
+  // 페이지 풀기
+  const flatPosts: Post[] =
+    data?.pages.map((item) => item.postList).flat() ?? [];
+
+  return (
+    <View style={styles.constainer}>
+      <InfiniteList
+        data={flatPosts}
+        error={error}
+        isLoading={isLoading}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isFetchingNextPage={isFetchingNextPage}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <FeedItem post={item} />}
+        ListEmptyComponent={MyCommentEmpty}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+  },
+});
