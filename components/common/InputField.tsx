28cc17156d685f25d770286fe8a5424fd33d22e3
Dessert99 <lsjh1234@naver.com>
2025-11-11 17:19:58 +0900

    feat(input): 유효성 검사 상태에 따른 UI 피드백 추가
    유효성 검사 결과(성공 또는 에러)에 따라 인풋 컴포넌트의
    스타일과 아이콘을 동적으로 변경합니다.
    
    - 성공: 체크 아이콘 표시 및 Green 테두리 적용
    - 에러: 에러 메시지 표시 및 Red 테두리 적용

--- components/common/InputField.tsx
+++ components/common/InputField.tsx
@@ -1,6 +1,7 @@
 import { colors } from "@/constants/colors";
 import React from "react";
 import {
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -25,22 +26,32 @@ function InputField({
       <Text style={styles.label}>{label}</Text>
       <View style={styles.container}>
         <TextInput
-          style={styles.input}
-          placeholderTextColor={colors.GRAY_200}
+          style={[
+            styles.input,
+            error && styles.errorInput,
+            isValid && styles.isVaildInput,
+          ]}
+          placeholderTextColor={colors.GRAY[400]}
           autoCapitalize="none" // 자동 대문자 제거
           spellCheck={false} // 입력값에 대한 맞춤법 검사 끄기
           autoCorrect={false} //입력값 자동 교정(오타 수정)을 끄기
           {...props}
         />
       </View>
-      {Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      {Boolean(error) && <Text style={styles.errorText}>{error}</Text>}
+      {isValid && (
+        <Image
+          style={styles.checkedImage}
+          source={require("@/assets/images/forA-checked.png")}
+        />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   label: {
-    color: colors.GRAY_300,
+    color: colors.GRAY[400],
     fontSize: 16,
   },
   container: {
@@ -50,10 +61,25 @@ const styles = StyleSheet.create({
   input: {
     borderBottomWidth: 2,
     fontSize: 16,
-    borderBottomColor: colors.GRAY_300,
+    borderBottomColor: colors.GRAY[400],
     paddingVertical: 8,
   },
-  error: {},
+  errorInput: {
+    borderBottomColor: colors.RED[400],
+  },
+  isVaildInput: {
+    borderBottomColor: colors.GREEN[400],
+  },
+  errorText: {
+    color: colors.RED[400],
+  },
+  checkedImage: {
+    position: "absolute",
+    right: 10,
+    top: 25,
+    width: 25,
+    height: 25,
+  },
 });
 
 export default InputField;
