244355ad2fb87f84a6a47f149b5fd2fdd8d858d2
Dessert99 <lsjh1234@naver.com>
2025-11-12 10:43:35 +0900

    feat(input): PasswordInput 컴포넌트 생성
    - 자체 유효성 검사 (required, minLength, pattern) 포함
    - fieldState(isDirty, invalid)에 따른 실시간 피드백 (success) 처리
    - secureTextEntry 등 비밀번호 관련 props 적용

--- components/auth/PasswordInput.tsx
+++ components/auth/PasswordInput.tsx
@@ -1,26 +1,49 @@
+import { REGEX } from "@/constants/valid";
 import React from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import InputField from "../common/InputField";
 
-interface PasswordInputProps {}
+interface PasswordInputProps {
+  label: string;
+  valueName: string;
+}
 
-function PasswordInput({}: PasswordInputProps) {
+function PasswordInput({ label, valueName }: PasswordInputProps) {
   const { control } = useFormContext();
+
   return (
     <Controller
-      name="password"
+      name={valueName}
       control={control}
-      render={({ field: { value, onChange } }) => {
+      render={({
+        field: { value, onChange },
+        fieldState: { invalid, error, isDirty },
+      }) => {
+        const success = !invalid && isDirty; // 유효성 통과 부울값
         return (
           <InputField
-            label="비밀번호를 입력해주세요."
+            label={label}
             placeholder="비밀번호를 입력해주세요."
             value={value}
-            onChangeText={(text) => onChange(text)}
+            isValid={success}
+            error={error?.message}
+            onChangeText={(text) => {
+              onChange(text.trim());
+            }}
             secureTextEntry // 암호화
             textContentType="oneTimeCode" //Strong Password 제거
+            maxLength={20}
+            returnKeyType="next"
           />
         );
+      }}
+      rules={{
+        required: "비밀번호를 입력해주세요.",
+        minLength: { value: 8, message: "8자 이상 입력해주세요." },
+        pattern: {
+          value: REGEX.PASSWORD,
+          message: "영문, 숫자, 특수문자 조합으로 입력해주세요.",
+        },
       }}
     />
   );
